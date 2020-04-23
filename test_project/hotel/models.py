20ba9399727312d66caef2c2054cd244e7b3a040
Aidar Garikhanov <a1d4r@yandex.ru>
2020-04-24 01:52:14 +0300

    Add basic models

--- test_project/hotel/models.py
+++ test_project/hotel/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class RoomCategory(models.Model):
+    hotel = models.ForeignKey('Hotel', on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+    min_price = models.IntegerField()
+
+
+class Room(models.Model):
+    room_category = models.ForeignKey('RoomCategory', on_delete=models.SET_NULL, null=True)
+
+
+class Booking(models.Model):
+    room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True)
+    date_check_in = models.DateTimeField()
+    date_check_out = models.DateTimeField()
+
