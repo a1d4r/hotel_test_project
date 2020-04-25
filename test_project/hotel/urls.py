5b8374bfd5cc5cc01cd026a41c7e4ad406719b09
Aidar Garikhanov <a1d4r@yandex.ru>
2020-04-25 18:29:40 +0300

    Implement user login, logout, change password

--- test_project/hotel/urls.py
+++ test_project/hotel/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 
 urlpatterns = [
     path('', views.index_page, name='index'),
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_page, name='logout'),
-    path('change_password/', views.change_password_page, name='change_password'),
+    path('change-password/', views.change_password_page, name='change_password'),
 ]
--- test_project/hotel/views.py
+++ test_project/hotel/views.py
@@ -1,17 +1,51 @@
-from django.shortcuts import render, HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 
 
+@login_required(login_url='login')
 def index_page(request):
-    return HttpResponse('Index page')
+    return render(request, 'hotel/index.html')
 
 
 def login_page(request):
-    return HttpResponse('Login page')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in!')
+            return redirect('index')
+        else:
+            messages.error(request, 'Incorrect username or password!')
+            return redirect('login')
+
+    form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, 'hotel/login.html', context)
 
 
+@login_required(login_url='login')
 def logout_page(request):
-    return HttpResponse('Logout page')
+    logout(request)
+    return redirect('login')
 
 
+@login_required(login_url='login')
 def change_password_page(request):
-    return HttpResponse('Change password page')
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password has been successfully updated!')
+            return redirect('index')
+        else:
+            messages.error(request, 'The old password is incorrect or new passwords do not match')
+
+    form = PasswordChangeForm(request.user)
+    context = {'form': form}
+    return render(request, 'hotel/change_password.html', context)
