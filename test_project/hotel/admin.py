1fb578e73d46959c5f6a22f5f80e6e6f718784f5
Aidar Garikhanov <a1d4r@yandex.ru>
2020-04-25 16:55:54 +0300

    Configure models, fill database from admin menu

--- test_project/hotel/admin.py
+++ test_project/hotel/admin.py
@@ -3,3 +3,7 @@ from .models import *
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Hotel)
+admin.site.register(RoomCategory)
+admin.site.register(Room)
+admin.site.register(Booking)
--- test_project/hotel/models.py
+++ test_project/hotel/models.py
@@ -6,9 +6,15 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     hotel = models.ForeignKey('Hotel', on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return self.user.username + " - " + str(self.hotel)
+
 
 class Hotel(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class RoomCategory(models.Model):
@@ -16,13 +22,26 @@ class RoomCategory(models.Model):
     name = models.CharField(max_length=100)
     min_price = models.IntegerField()
 
+    def __str__(self):
+        return str(self.hotel) + " - " + self.name + " - " + str(self.min_price)
+
+    class Meta:
+        verbose_name_plural = 'Room categories'
+
 
 class Room(models.Model):
     room_category = models.ForeignKey('RoomCategory', on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+        return str(self.room_category.hotel) + " - " + str(self.name)
 
 
 class Booking(models.Model):
     room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True)
     date_check_in = models.DateTimeField()
     date_check_out = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.room)
 
