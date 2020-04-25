2fc2714e76cf1099c4baa5c1faa0094a9781b7a1
Aidar Garikhanov <a1d4r@yandex.ru>
2020-04-25 17:02:24 +0300

    Configure urls for authorization pages

--- test_project/hotel/urls.py
+++ test_project/hotel/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index_page, name='index'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
+    path('change_password/', views.change_password_page, name='change_password'),
 ]
--- test_project/hotel/views.py
+++ test_project/hotel/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def index_page(request):
+    return HttpResponse('Index page')
+
+
+def login_page(request):
+    return HttpResponse('Login page')
+
+
+def logout_page(request):
+    return HttpResponse('Logout page')
+
+
+def change_password_page(request):
+    return HttpResponse('Change password page')
